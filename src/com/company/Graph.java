efea2b4dc279ce01576201586f8d16c5614297ec
timofeyreedtz <myz-timofey@mail.ru>
2022-04-16 15:17:30 +0300

    Algorithm for uniform data distribution in a network represented as a directed graph with cycle processing

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 class GraphNode{
     private int from,to;
@@ -49,32 +50,91 @@ public class Graph {
     private int num_of_tops, num_of_weights;
     private ArrayList<GraphNode> graph = new ArrayList<>();
     private final double[] size_array;
+    private final int[] count;
+    private final boolean[] isChanged;
+    private final double[] size_array_for_cycle_points;
     private final double[] tops_array_from;
     Graph(int num_of_tops, int num_of_weights){
         this.num_of_tops = num_of_tops;
         this.num_of_weights = num_of_weights;
         size_array = new double[this.num_of_tops];
+        count = new int[this.num_of_tops];
+        size_array_for_cycle_points = new double[this.num_of_tops];
         tops_array_from = new double[this.num_of_tops];
+        isChanged = new boolean[this.num_of_tops];
         for(int i = 0;i < num_of_tops;i++){
             tops_array_from[i] = 0;
+            isChanged[i] = false;
+            count[i] = 0;
         }
     }
-    private  void FillTopsArr() {
+    private  void FillTopsArr(double [] arr) {
         for (int i = 0; i < num_of_weights; i++) {
-            tops_array_from[graph.get(i).getFrom()-1]++;
+            arr[graph.get(i).getFrom()-1]++;
         }
     }
     public void OptimizeSize(double V){
         size_array[0] = V;
+        FillTopsArr(tops_array_from);
+        Optimize();
+    }
+
+    private void Optimize() {
         double size = 0;
-        FillTopsArr();
-        for (int i = 0;i < num_of_weights;i++){
-            size  = size_array[graph.get(i).getFrom()-1]/tops_array_from[graph.get(i).getFrom()-1] ;
-            if(size > graph.get(i).getWeight()) graph.get(i).setWeight(size);
-            size_array[graph.get(i).getTo()-1] += size;
-            graph.get(i).setSize(size);
+        int from = 0;
+        for (int i = 0 ; i < num_of_weights; i++){
+            if ((graph.get(i).getFrom() > graph.get(i).getTo())){ // проверка на цикл
+                size  = size_array[graph.get(i).getFrom()-1]/tops_array_from[graph.get(i).getFrom()-1] ; // находим объем в вершине
+                tops_array_from[graph.get(i).getFrom()-1]--; //  уменьшаем количество исходящих путей в вершине на 1
+                if(size > graph.get(i).getWeight()) graph.get(i).setWeight(size);
+                size_array_for_cycle_points[graph.get(i).getTo()-1] = size; // отправляем полученный объем в вершину, в которой образуется новый цикл
+
+                size_array[graph.get(i).getFrom()-1] -= size; // уменьшаем объем в текущей вершине на величину отправленного объёма
+                for(int k = 0;k < num_of_weights;k++){
+                    if(graph.get(i).getTo() == graph.get(k).getFrom()) // ищем узел, с которого начнем следующий цикл
+                        from = k;
+                        break;
+                }
+                for(int k = from;k < num_of_weights;k++){
+                    while(graph.get(k).getFrom() > graph.get(k).getTo()){ /*
+                    проверка на цикл: если нашли необработанный цикл, игнорируем его, но уменьшаем кол-во исходящих путей в нем на 1*/
+                        if(k > i){
+                            tops_array_from[graph.get(k).getFrom()-1]--;
+                            isChanged[graph.get(k).getFrom()-1] = true;
+                            count[graph.get(k).getFrom()-1]++;
+                        }
+                        k++;
+
+                    }/*
+                    распределяем объем в нужные вершины
+                    */
+                    size  = size_array_for_cycle_points[graph.get(k).getFrom()-1]/tops_array_from[graph.get(k).getFrom()-1] ;
+                    if(size > graph.get(k).getWeight()) graph.get(k).setWeight(size);
+                    size_array_for_cycle_points[graph.get(k).getTo()-1] += size;
+                }
+                /*
+                суммируем объем, прошедший через цикл с объемом в конечной вершине графа
+                 */
+                size_array[num_of_tops-1] += size_array_for_cycle_points[num_of_tops-1];
+                for (int k = 0;k < num_of_tops;k++){
+                    size_array_for_cycle_points[k] = 0;
+                    /*
+                    возвращаем исходное количество вершин в необработанных циклах
+                    */
+                    if(isChanged[k]){
+                        tops_array_from[k] += count[k];
+                        isChanged[k] = false;
+                        count[k] = 0;
+                    }
+                }
+            }
+            else{ // распределение объема
+                size  = size_array[graph.get(i).getFrom()-1]/tops_array_from[graph.get(i).getFrom()-1] ;
+                if(size > graph.get(i).getWeight()) graph.get(i).setWeight(size);
+                size_array[graph.get(i).getTo()-1] += size;
+                graph.get(i).setSize(size);
+            }
         }
-        graph.get(num_of_weights-1).setSize(size);
     }
     public void addGraphNode(int from, int to, double weight, double size){
         graph.add(new GraphNode(from,to,weight,size));
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,30 +5,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Graph g = new Graph(9,17);
+        Graph g = new Graph(6,17);
         g.addGraphNode(1,2,5,0);
-        g.addGraphNode(1,4,5,0);
         g.addGraphNode(1,3,5,0);
+        g.addGraphNode(2,1,5,0);
         g.addGraphNode(2,4,5,0);
-        g.addGraphNode(2,7,5,0);
-        g.addGraphNode(3,4,5,0);
+        g.addGraphNode(3,1,5,0);
+        g.addGraphNode(3,2,5,0);
         g.addGraphNode(3,5,5,0);
-        g.addGraphNode(3,6,5,0);
+        g.addGraphNode(4,1,5,0);
+        g.addGraphNode(4,2,5,0);
+        g.addGraphNode(4,3,5,0);
+        g.addGraphNode(4,5,5,0);
         g.addGraphNode(4,6,5,0);
-        g.addGraphNode(4,7,5,0);
+        g.addGraphNode(5,1,5,0);
+        g.addGraphNode(5,2,5,0);
+        g.addGraphNode(5,3,5,0);
+        g.addGraphNode(5,4,5,0);
         g.addGraphNode(5,6,5,0);
-        g.addGraphNode(5,8,5,0);
-        g.addGraphNode(6,7,3,0);
-        g.addGraphNode(6,8,3,0);
-        g.addGraphNode(6,9,4,0);
-        g.addGraphNode(7,9,3,0);
-        g.addGraphNode(8,9,3,0);
         Scanner s = new Scanner(System.in);
         System.out.println("Введите входной объем данных: ");
         double V = s.nextDouble();
         System.out.println("Входной объем данных: " + V);
         g.OptimizeSize(V);
-        g.printGraph();
         g.printArray();
     }
 }
