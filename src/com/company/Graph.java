072676bbe8620daaad3f41b42588579bcffee4fa
timofeyreedtz <myz-timofey@mail.ru>
2022-04-30 12:27:50 +0300

    Adding Ford-Fulkerson max_flow algorithm and time of data processing

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -1,17 +1,20 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.*;
 
 class GraphNode{
     private int from,to;
-    private double weight;
+    private double weight,size,delay ;
 
-    GraphNode(int from, int to, double weight){
+    GraphNode(int from, int to, double weight,double size,double delay){
         this.from = from;
         this.to = to;
         this.weight = weight;
-
+        this.size = size;
+        this.delay = delay;
     }
     public int getFrom() {
         return from;
@@ -36,140 +39,135 @@ class GraphNode{
     public void setWeight(double weight) {
         this.weight = weight;
     }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getDelay() {
+        return delay;
+    }
+
+    public void setDelay(double delay) {
+        this.delay = delay;
+    }
 }
 public class Graph {
     private int num_of_tops, num_of_weights;
     private ArrayList<GraphNode> graph = new ArrayList<>();
+    private final ArrayList<Thread> thread = new ArrayList<>();
     private final double[] size_array;
-    private final int[] count;
-    private final boolean[] isChanged;
-    private final double[] size_array_for_cycle_points;
-    private final double[] tops_array_from;
+    private final double[][] matrix;
+    private final int[] tops_array_from;
     Graph(int num_of_tops, int num_of_weights){
         this.num_of_tops = num_of_tops;
         this.num_of_weights = num_of_weights;
         size_array = new double[this.num_of_tops];
-        count = new int[this.num_of_tops];
-        size_array_for_cycle_points = new double[this.num_of_tops];
-        tops_array_from = new double[this.num_of_tops];
-        isChanged = new boolean[this.num_of_tops];
+        matrix = new double[this.num_of_tops][this.num_of_tops];
+        tops_array_from = new int[this.num_of_tops];
         for(int i = 0;i < num_of_tops;i++){
             tops_array_from[i] = 0;
-            isChanged[i] = false;
-            count[i] = 0;
+            for (int j = 0;j < num_of_tops;j++){
+                matrix[i][j] = 0;
+            }
         }
+
     }
-    private  void FillTopsArr(double [] arr) {
+    private void maxFlow(){
+        System.out.format("Максимальны поток из начальной в конечную вершины = %.1f\n"
+                ,MaxFlow_Ford_Fulkerson.findMaxFlow(matrix,0,num_of_tops-1,num_of_tops));
+    }
+    private  void FillTopsArr(int [] arr) {
         for (int i = 0; i < num_of_weights; i++) {
             arr[graph.get(i).getFrom()-1]++;
         }
     }
+
     public void OptimizeSize(double V){
         size_array[0] = V;
         FillTopsArr(tops_array_from);
-        Optimize();
+        deleteLoops();
+        Run();
+        maxFlow();
     }
 
-    private void Optimize() {
-        double size = 0;
-        int from = 0;
-        for (int i = 0 ; i < num_of_weights; i++){
-            if ((graph.get(i).getFrom() > graph.get(i).getTo())){ // проверка на цикл
-                size  = size_array[graph.get(i).getFrom()-1]/tops_array_from[graph.get(i).getFrom()-1] ; // находим объем в вершине
-                tops_array_from[graph.get(i).getFrom()-1]--; //  уменьшаем количество исходящих путей в вершине на 1
-                while (size > graph.get(i).getWeight()){ // обработки циклов в случае если недостачи пропускной способности канала
-                    size -= graph.get(i).getWeight();
-                    System.out.println("from " + graph.get(i).getFrom()  + " to " + graph.get(i).getTo()  + ": "+ graph.get(i).getWeight());
-                    size_array_for_cycle_points[graph.get(i).getTo()-1] = graph.get(i).getWeight();;
-                    System.out.println("Loop start!");
-                    loopProcessing(graph.get(i).getWeight(),i);
-                    size_array[graph.get(i).getFrom()-1] -= graph.get(i).getWeight();
-                    System.out.println("Loop end!");
-                }
-                System.out.println("from " + graph.get(i).getFrom() + " to " + graph.get(i).getTo() + ": "+ size);
-                size_array_for_cycle_points[graph.get(i).getTo()-1] = size; // отправляем полученный объем в вершину, в которой образуется новый цикл
-                size_array[graph.get(i).getFrom()-1] -= size; // уменьшаем объем в текущей вершине на величину отправленного объёма
-                System.out.println("Loop start!");
-                loopProcessing(size,i);
-                System.out.println("Loop end!");
-            }
-            else{ // распределение объема
-
-                size  = size_array[graph.get(i).getFrom()-1]/tops_array_from[graph.get(i).getFrom()-1] ;
-                checkIfOverload(size,i,size_array); // обработка в случае нехватки пропускной способности канала
-                System.out.println("from " + graph.get(i).getFrom()  + " to " + graph.get(i).getTo()  + ": "+ size);
-                size_array[graph.get(i).getTo()-1] += size;
-                if(i == num_of_weights-1){
-                    size_array[graph.get(i).getFrom()-1] = 0;
-                }
-                else if (graph.get(i).getFrom() != graph.get(i+1).getFrom()){
-                    size_array[graph.get(i).getFrom()-1] = 0;
-                }
-            }
-        }
-    }
-
-    private void loopProcessing(double size, int i) {
-        int from = 0;
-        for(int k = 0;k < num_of_weights;k++){
-            if(graph.get(i).getTo() == graph.get(k).getFrom()) // ищем узел, с которого начнем следующий цикл
-                from = k;
-                break;
-        }
-        for(int k = from; k < num_of_weights; k++){
-            while(graph.get(k).getFrom()-1 > graph.get(k).getTo()-1){ /*
-            проверка на цикл: если нашли необработанный цикл, игнорируем его, но уменьшаем кол-во исходящих путей в нем на 1*/
-                if(k > i){
-                    tops_array_from[graph.get(k).getFrom()-1]--;
-                    isChanged[graph.get(k).getFrom()-1] = true;
-                    count[graph.get(k).getFrom()-1]++;
-                }
+    private int calcTops(int i){
+        int k = 0;
+        for (int j = 0;j < num_of_weights; j++){
+            if (graph.get(i).getFrom() == graph.get(j).getFrom()){
                 k++;
-                if(k == num_of_weights){
-                    break;              // если дошли до последнего узла - выходим
-                }
-
-            }/*
-            распределяем объем в нужные вершины
-            */
-            if(k == num_of_weights){  // если дошли до последнего узла - выходим
-                break;
-            }
-            checkIfOverload(size, i,size_array_for_cycle_points);
-            size = size_array_for_cycle_points[graph.get(k).getFrom()-1]/tops_array_from[graph.get(k).getFrom()-1] ;
-            System.out.println("from " + graph.get(k).getFrom() + " to " + graph.get(k).getTo() + ": "+ size);
-            size_array_for_cycle_points[graph.get(k).getTo()-1] += size;
-        }
-        size_array[num_of_tops-1] += size_array_for_cycle_points[num_of_tops-1];
-        for (int k = 0;k < num_of_tops;k++){
-            size_array_for_cycle_points[k] = 0;
-                    /*
-                    возвращаем исходное количество вершин в необработанных циклах
-                    */
-            if(isChanged[k]){
-                tops_array_from[k] += count[k];
-                isChanged[k] = false;
-                count[k] = 0;
             }
         }
-        if(i == num_of_weights-1){
-            size_array_for_cycle_points[graph.get(i).getFrom()-1] = 0;
+        return k;
+    }
+    private void deleteLoops(){
+        for(int i = 0;i < num_of_weights;i++){
+            if ((graph.get(i).getFrom() > graph.get(i).getTo())){
+                deleteGraphNode(i);
+                tops_array_from[graph.get(i).getFrom()-1]--;
+                num_of_weights--;
+            }
         }
-        else if (graph.get(i).getFrom() != graph.get(i+1).getFrom()){
-            size_array_for_cycle_points[graph.get(i).getFrom()-1] = 0;
+    }
+    private void Run()  {
+        int k = 0;
+        for(int i = 0;i < num_of_weights;i++){
+            int finalI = i;
+            thread.add(new Thread(() -> Optimize(finalI)));
+        }
+        for(int i = 0;i < num_of_tops;i++){
+            int to = tops_array_from[i];
+            if(i == 0){
+                k = 0;
+            }
+            else k+=calcTops(k);
+            try {
+                Thread.sleep((long) graph.get(graph.get(i).getFrom()-1).getDelay());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for(int j = k; j < k + to; j++){
+                thread.get(j).start();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private void checkIfOverload(double size, int i,double [] arr) {
+    private  void Optimize(int i) {
+        double size;
+        size  = size_array[graph.get(i).getFrom()-1]/calcTops(i) ;
+        graph.get(i).setSize(size);
+        matrix[graph.get(i).getFrom()-1][graph.get(i).getTo()-1] = size;
+        graph.get(i).setDelay(size*1000);
+        size  = checkIfOverload(size,i,size_array); // обработка в случае нехватки пропускной способности канала
+        System.out.println("from " + graph.get(i).getFrom()  + " to " + graph.get(i).getTo()  + ": "+ size);
+        size_array[graph.get(i).getTo()-1] += size;
+        if(i == num_of_weights-1){
+            size_array[graph.get(i).getFrom()-1] = 0;
+        }
+        else if (graph.get(i).getFrom() != graph.get(i+1).getFrom() ){
+            size_array[graph.get(i).getFrom()-1] = 0;
+        }
+    }
+    private double checkIfOverload(double size, int i,double [] arr) {
         while (size > graph.get(i).getWeight()){
             size -= graph.get(i).getWeight();
             System.out.println("from " + graph.get(i).getFrom()  + " to " + graph.get(i).getTo()  + ": "+ graph.get(i).getWeight());
             arr[graph.get(i).getTo()-1] += graph.get(i).getWeight();
-        }
-    }
 
-    public void addGraphNode(int from, int to, double weight){
-        graph.add(new GraphNode(from,to,weight));
+        }
+        return size;
+    }
+    public void addGraphNode(int from, int to, double weight,double size,double delay){
+        graph.add(new GraphNode(from,to,weight,size,delay));
     }
     public void deleteGraphNode(int i){
         graph.remove(i);
@@ -177,21 +175,17 @@ public class Graph {
     public ArrayList<GraphNode> getGraph() {
         return graph;
     }
-
     public void setGraph(ArrayList<GraphNode> graph) {
         this.graph = graph;
     }
     public void printGraph(){
         for (int i = 0; i<num_of_weights;i++){
             System.out.format("{ %d o---------o %d" +
-                            "   пропускная способность канала: %.3f\n"
+                            "   пропускная способность канала: %.3f" +
+                            "   время обработки: %.3f" +
+                            "   прошедший объем: %.3f\n"
                             ,graph.get(i).getFrom()
-                    ,graph.get(i).getTo(),graph.get(i).getWeight());
-        }
-    }
-    public void printArray(){
-        for (int i = 0; i < num_of_tops;i++){
-            System.out.format("Объем данных в %d вершине: %.3f\n" ,i+1, size_array[i]);
+                    ,graph.get(i).getTo(),graph.get(i).getWeight(),graph.get(i).getDelay(),graph.get(i).getSize());
         }
     }
     public int getNum_of_tops() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Graph g = new Graph(5,9);
-        g.addGraphNode(1,2,3);
-        g.addGraphNode(1,3,3);
-        g.addGraphNode(2,1,1);
-        g.addGraphNode(2,4,3);
-        g.addGraphNode(3,1,1);
-        g.addGraphNode(3,5,3);
-        g.addGraphNode(4,1,1);
-        g.addGraphNode(4,2,0.2);
-        g.addGraphNode(4,5,5);
-
+        Graph g = new Graph(7,12);
+        g.addGraphNode(1,2,4,0,0);
+        g.addGraphNode(1,3,4,0,0);
+        g.addGraphNode(1,4,4,0,0);
+        g.addGraphNode(2,3,4,0,0);
+        g.addGraphNode(2,6,4,0,0);
+        g.addGraphNode(3,4,4,0,0);
+        g.addGraphNode(3,5,4,0,0);
+        g.addGraphNode(3,7,1,0,0);
+        g.addGraphNode(4,5,4,0,0);
+        g.addGraphNode(5,1,4,0,0);
+        g.addGraphNode(5,7,4,0,0);
+        g.addGraphNode(6,7,4,0,0);
         Scanner s = new Scanner(System.in);
         System.out.println("Введите входной объем данных: ");
         double V = s.nextDouble();
         System.out.println("Входной объем данных: " + V);
+        long start = System.nanoTime();
         g.OptimizeSize(V);
-        g.printArray();
+        long finish = System.nanoTime();
+        long elapsed = finish - start;
+        System.out.format("Прошло времени, нс: %.5f\n" ,(double)elapsed/ 1_000_000_000);
+        g.printGraph();
     }
 }
